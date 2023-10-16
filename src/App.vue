f4d5473e8ea0ba02df15c44f48740eda9b0f73cd
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2023-10-16 14:28:59 -0300

    Update home style.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
-    <div>
-        <h1>JSON Tree Viewer</h1>
-        <p>Simple JSON Viewer that runs completely on-client. No data exchange</p>
+        <div
+            v-if="!jsonLoaded"
+            class="home"
+        >
+            <h1>JSON Tree Viewer</h1>
+            <p>Simple JSON Viewer that runs completely on-client. No data exchange</p>
 
-        <button @click="triggerFileInput">Load JSON</button>
-        <input type="file" id="fileInput" @change="loadJSON" accept=".json" style="display: none;" />
-        
-        <div class="tree-viewer">
-            <JsonTreeView
-                v-if="jsonLoaded"
-                :node="jsonContent"
-                :first="true"
+            <button @click="triggerFileInput">Load JSON</button>
+            <input
+                type="file"
+                id="fileInput"
+                @change="loadJSON"
+                accept=".json"
+                style="display: none;"
             />
         </div>
-    </div>
+        <div
+            v-if="jsonLoaded"
+            class="tree-viewer-container"
+        >
+            <div class="tree-viewer">
+                <h1>{{ jsonFileName }}</h1>
+                <JsonTreeView
+                    :node="jsonContent"
+                    :first="true"
+                />
+            </div>
+        </div>
 </template>
 
 <script setup>
@@ -21,6 +34,7 @@ import { ref } from 'vue';
 import JsonTreeView from './components/JsonTreeView.vue';
 
 let jsonContent = ''
+let jsonFileName = ''
 let jsonLoaded = ref(false)
 
 function triggerFileInput()
@@ -40,6 +54,7 @@ function loadJSON(event)
     reader.onload = function (event)
     {
         jsonContent = JSON.parse(event.target.result)
+        jsonFileName = file.name
         jsonLoaded.value = true
     }
 
@@ -48,8 +63,28 @@ function loadJSON(event)
 </script>
 
 <style>
+.tree-viewer-container {
+    display: flex;
+    justify-content: center;
+}
+
 .tree-viewer {
     margin-top: 30px;
     line-height: 1.75rem;
+}
+
+.home {
+    position: absolute;
+    top: 45%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    text-align: center;
+}
+.home button {
+    margin-top: .5rem;
+    background: linear-gradient(180deg, #E4E4E4 0%, #F7F7F7 100%);
+    border: 1px solid black;
+    padding: 0.25rem .75rem;
+    border-radius: .315rem;
 }
 </style>
